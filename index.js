e5fb253012998db91f0f6da37d1fe79fb71ccb43
SuperNova-1605 <ardinova0503@gmail.com>
2025-02-22 18:42:15 +0100

    Leads Tracker App

---
+++ index.js
@@ -0,0 +1,53 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"
+import { getDatabase,
+         ref,
+         push,
+        onValue,
+        remove } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
+
+const firebaseConfig = {
+    databaseURL: "https://leads-tracker-app-fb69f-default-rtdb.europe-west1.firebasedatabase.app/"
+
+}
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+const referenceInDB = ref(database, "leads")
+
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    remove(referenceInDB);
+    ulEl.innerHTML = "";
+})
+
+onValue(referenceInDB, function(snapshot) {
+    const snapshotExists = snapshot.exists();
+    if (snapshotExists) {
+        const snapshotValues = snapshot.val();
+        const leads = Object.values(snapshotValues);
+        render(leads);
+    }
+} )
+
+inputBtn.addEventListener("click", function() {
+    push(referenceInDB, inputEl.value)
+    inputEl.value = ""
+})
